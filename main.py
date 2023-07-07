29e6978639370ea93229e712edc7f650ba54c97d
Jalen-B001 <124908144+Jalen-B001@users.noreply.github.com>
2023-07-07 08:23:28 +0000

    Submission Confirmation

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 #your code here!
+miles_driven = input("Enter the amount of miles driven: ")
+gallons_used = input("Enter how many gallons you've used to fill in your gas tank: ")
+miles_per_gallon = float(miles_driven) / float(gallons_used)
+print("You've recieved", miles_per_gallon, 'miles per gallon on your last tank refill')
 
 #read in the miles and read in the gallons used.
 
